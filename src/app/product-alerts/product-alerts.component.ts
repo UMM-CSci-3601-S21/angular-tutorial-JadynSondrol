f434401840cc0abda22e842cb9492b5e2c5dfbeb
Jadyn <sondr037@morris.umn.edu>
2021-02-06 12:39:25 -0600

    define a property named product
    indicates that the property value passes in from the component's parent, (ProductListComponent)

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -7,10 +7,10 @@ import { Input } from '@angular/core';
   styleUrls: ['./product-alerts.component.css']
 })
 export class ProductAlertsComponent implements OnInit {
-
+  @Input() product;
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
 }
