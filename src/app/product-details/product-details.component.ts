44aae9c186b52ba87641c68a35643804733de04e
Jadyn <sondr037@morris.umn.edu>
2021-02-07 16:13:38 -0600

    extract the product Id from the route parameters
    and find the corresponding product in the products array

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
 import { products } from '../products';
 
 @Component({
@@ -15,7 +14,15 @@ export class ProductDetailsComponent implements OnInit {
     private route: ActivatedRoute,
   ) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    // First get the product id from the current route.
+    const routeParams = this.route.snapshot.paramMap;
+    const productIdFromRoute =
+    Number(routeParams.get('productId'));
+
+    //Find the product that corresponds with the id provided in route.
+    this.product = products.find(product => product.id ===
+      productIdFromRoute);
   }
 
 }
