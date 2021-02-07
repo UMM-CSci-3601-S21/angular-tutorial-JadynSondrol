18ef4be867920856c024e52f9c3f24ed21d639ca
Jadyn <sondr037@morris.umn.edu>
2021-02-07 16:58:00 -0600

    add a route for the component CartComponent
    with a path of cart

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,7 +18,8 @@ import { CartComponent } from './cart/cart.component';
       { path: '', component: ProductListComponent },
       { path: 'products/:productId', component:
     ProductDetailsComponent}
-    ], { useHash: true })
+      { path: 'cart', component: CartComponent },
+    ])
   ],
   declarations: [
     AppComponent,
