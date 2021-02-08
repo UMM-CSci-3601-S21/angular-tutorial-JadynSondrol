429fea2ace071ad6e940ccd81201f1ceae8542db
Jadyn <sondr037@morris.umn.edu>
2021-02-07 18:15:25 -0600

    fix the route for the Cart component
    add a ,

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,7 +17,7 @@ import { CartComponent } from './cart/cart.component';
     RouterModule.forRoot([
       { path: '', component: ProductListComponent },
       { path: 'products/:productId', component:
-    ProductDetailsComponent}
+    ProductDetailsComponent},
       { path: 'cart', component: CartComponent },
     ])
   ],
